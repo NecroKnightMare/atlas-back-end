7254f59eb98aed494b693e6fe8dcfa07f13c921f
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-11-11 22:26:38 -0600

    shuffled code around and changed method and variable names

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -8,49 +8,51 @@ import requests
 import sys
 
 
-def get_employee_todo(employee_id):
+def get_employee_data(employee_id):
     """
     get employee details
     Args:
         employee_id (_type_)
     """
-    employee_url = f"https://jsonplaceholder.typicode.com" \
+    user_url = f"https://jsonplaceholder.typicode.com" \
         f"/users/{employee_id}"
-    response = requests.get(employee_url)
+    todo_url = f"https://jsonplaceholder.typicode.com" \
+        f"/todos?userId={employee_id}"
 
-    if response.status_code != 200:
-        print("Failed to fetch employee details")
-        return
+    user_response = requests.get(user_url)
+    todo_response = requests.get(todo_url)
 
-    employee_data = response.json()
-    employee_name = employee_data['name']
+    if user_response.status_code != 200 or todo_response:
+        print("Error: Unable to fetch employee details")
+        sys.exit(1)
 
-    todos_url = f"https://jsonplaceholder.typicode.com/" \
-        f"todos?userID={employee_id}"
+    user_data = user_response.json()
+    todo_data = todo_response.json()
 
-    response = requests.get(todos_url)
-    if response.status_code != 200:
-        print("Failed to fetch TODO list")
-        return
+    return user_data, todo_data
 
-    todos_data = response.json()
-    total_tasks = len(todos_data)
-    done_tasks = [task for task in todos_data if task['completed']]
-    number_of_done_tasks = len(done_tasks)
+
+def show_progress(employee_id):
+    user_data, todo_data = get_employee_data(employee_id)
+
+    employee_name = user_data['name']
+    total_tasks = len(todo_data)
+    completed_tasks = [task for task in todo_data if task['completed']]
+    done_tasks = len(completed_tasks)
 
     print(f"Employee {employee_name} is done with tasks"
-          f"({number_of_done_tasks}/{total_tasks}):")
-    for task in done_tasks:
+          f"({done_tasks}/{total_tasks}):")
+    for task in completed_tasks:
         print(f"\t {task['title']}")
 
     if __name__ == "__main__":
         if len(sys.argv) != 2:
-            print("Usage: python3 script.py <employee_id>")
+            print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
             sys.exit(1)
 
         try:
             employee_id = int(sys.argv[1])
-            get_employee_todo(employee_id)
+            get_employee_data(employee_id)
         except ValueError:
-            print("Employee id must be an integer")
+            print("Error: Employee id must be an integer")
             sys.exit(1)
