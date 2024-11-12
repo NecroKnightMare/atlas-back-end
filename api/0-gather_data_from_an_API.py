f72cf769c0bf71b6bdbc8c73d4c677e9ce56dec1
Ariel.Lopez <alexandrea.lopez@atlasschool.com>
2024-11-11 22:52:29 -0600

    switched it up ageain, deleted other method to use variables

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -20,39 +20,25 @@ def get_employee_data(employee_id):
         f"/todos?userId={employee_id}"
 
     user_response = requests.get(user_url)
-    todo_response = requests.get(todo_url)
-
-    if user_response.status_code != 200 or todo_response:
-        print("Error: Unable to fetch employee details")
-        sys.exit(1)
-
+    if user_response.status_code != 200:
+        print(f"Error: {employee_id}")
+        return
     user_data = user_response.json()
+    
+    todo_response = requests.get(todo_url)
+    if todo_response.status_code != 200:
+        print(f"Error: todo {employee_id}")
+        return
     todo_data = todo_response.json()
-
-    return user_data, todo_data
-
-
-def show_progress(employee_id):
-    user_data, todo_data = get_employee_data(employee_id)
 
     employee_name = user_data.get("username")
     total_tasks = len(todo_data)
     completed_tasks = [task for task in todo_data if task['completed']]
-    done_tasks = len(completed_tasks)
 
     print(f"Employee {employee_name} is done with tasks"
-          f"({done_tasks}/{total_tasks}):")
+          f"({completed_tasks}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t {task['title']}")
 
     if __name__ == "__main__":
-        if len(sys.argv) != 2:
-            print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-            sys.exit(1)
-
-        try:
-            employee_id = int(sys.argv[1])
-            get_employee_data(employee_id)
-        except ValueError:
-            print("Error: Employee id must be an integer")
-            sys.exit(1)
+        get_employee_data(int(sys.argv[1]))
