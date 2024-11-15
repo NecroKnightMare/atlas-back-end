8d3b9ff2e06e4aa978aae8048b34493f27f5d26e
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-15 04:57:44 +0000

    reverted

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -16,38 +16,38 @@ def get_employee_progress(employee_id):
     """
     base_url = f"https://jsonplaceholder.typicode.com"
 
-    # if not isinstance(employee_id, int):
-    #     raise TypeError("employee_id must be an integer.")
+    if not isinstance(employee_id, int):
+        raise TypeError("employee_id must be an integer.")
 
-    # employee_url = f"{base_url}/users/{employee_id}"
-    # employee_response = requests.get(employee_url).json()
+    employee_url = f"{base_url}/users/{employee_id}"
+    employee_response = requests.get(employee_url).json()
 
-    # if employee_response.status_code == 200:
-    #     employee_data = employee_response.json()
-    # else:
-    #     print(f"Invalid emlpoyee id")
-    #     return
+    if employee_response.status_code == 200:
+        employee_data = employee_response.json()
+    else:
+        print(f"Invalid emlpoyee id")
+        return
 
-    # todo_url = f"{base_url}/todos?userId={employee_id}"
-    # todo_response = requests.get(todo_url).json()
+    todo_url = f"{base_url}/todos?userId={employee_id}"
+    todo_response = requests.get(todo_url).json()
 
-    # if todo_response.status_code == 200:
-    #     todo_data = todo_response.json()
-    # else:
-    #     print(f"No todo list found")
-    #     return
+    if todo_response.status_code == 200:
+        todo_data = todo_response.json()
+    else:
+        print(f"No todo list found")
+        return
 
-    # employee_name = employee_response.get("username")
-    # total_tasks = len(todo_data)
-    # completed_tasks = [task.get("title")
-    #                    for task in todo_data if task.get("completed")]
+    employee_name = employee_response.get("username")
+    total_tasks = len(todo_data)
+    completed_tasks = [task.get("title")
+                       for task in todo_data if task.get("completed")]
 
-    # print(f"Employee {employee_name} is done with tasks"
-    #       f"({len(completed_tasks)}/{total_tasks}):")
-    # for task in completed_tasks:
-    #     print(f"\t {task}")
+    print(f"Employee {employee_name} is done with tasks"
+          f"({len(completed_tasks)}/{total_tasks}):")
+    for task in completed_tasks:
+        print(f"\t {task}")
 
-    user_response = requests.get("{}/users/{}".format(base_url, employee_id))
+   user_response = requests.get("{}/users/{}".format(base_url, employee_id))
     if user_response.status_code != 200:
         print("Error fetching user with ID {}".format(employee_id))
         return
@@ -73,16 +73,16 @@ def get_employee_progress(employee_id):
     for task in done_tasks:
         print(f"\t {task.get('title')}")
 
+
     if __name__ == "__main__":
         if len(sys.argv) != 2:
             print("Usage: employee id")
             sys.exit(1)
-        else:
-            try:
-                employee_id = int(sys.argv[1])
-                get_employee_progress(employee_id)
-            except ValueError:
-                print("Provide correct employee name")
-                sys.exit(1)
 
-        # get_employee_progress(int(sys.argv[1]))
+        try:
+            employee_id = int(sys.argv[1])
+        except ValueError:
+            print("Provide correct employee name")
+            sys.exit(1)
+
+        get_employee_progress(int(sys.argv[1]))
