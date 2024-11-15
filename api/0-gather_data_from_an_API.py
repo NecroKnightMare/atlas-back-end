630a7fb2ecba5733cd1460be0933a0421b08d7e5
Ariel <140107544+NecroKnightMare@users.noreply.github.com>
2024-11-15 04:21:48 +0000

    indent

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -45,10 +45,10 @@ def display_employee_progress(employee_name, todo):
     TODO progress
     """
     completed_tasks = [todo["title"] for todo in todos if
-            todo["completed"]]
+        todo["completed"]]
     total_tasks = len(todos)
     print(f"Employee {employee_name} is done with"
-            f"tasks({len(completed_tasks)}/{total_tasks}):")
+        f"tasks({len(completed_tasks)}/{total_tasks}):")
     for task in completed_tasks:
         print(f"\t {task}")
 
